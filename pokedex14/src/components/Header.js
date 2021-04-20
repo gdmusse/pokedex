3effa7608af4a66005a60f23757eb6608c45e77d
Gabriel Musse <gdmusse@hotmail.com>
2021-04-20 15:27:03 -0300

    arrumei header

--- pokedex14/src/components/Header.js
+++ pokedex14/src/components/Header.js
@@ -1,55 +1,87 @@
 import { useHistory } from "react-router-dom";
 import styled from "styled-components";
-import Logo from "../images/logo.png"
-import {goToHomePage, goToPokedex} from "../routes/coordinator"
+import Logo from "../images/logo.png";
+import { goToHomePage, goToPokedex } from "../routes/coordinator";
 
 const HeaderDiv = styled.div`
-background-color: #EF5350;
-height: 150px;
-display: flex;
-justify-content: space-between;
-`
+  background-color: #ef5350;
+  height: 150px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const HeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 80%;
+  margin: 0 auto;
+`;
 
 const TitleContainer = styled.div`
-
   display: flex;
   justify-content: center;
   align-items: center;
   color: white;
+  font-weight: bold;
+  font-size: 40px;
 `;
 
-const DivImagens = styled.div `
-display:flex;
-width: 400px;
-& > img:hover  {
+const ImgContainer = styled.div`
+  display: flex;
+  flex: 1;
+
+  justify-content: flex-end;
+  & > img:hover {
     cursor: pointer;
-}
-`
+  }
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+
+  flex: 1;
+`;
+const LogoImg = styled.img`
+  width: auto;
+  height: 100px;
+`;
 
 const Imagem1 = styled.img`
-
-
-`
+  width: auto;
+  height: 100px;
+`;
 
 const Imagem2 = styled.img`
-
-`
+  width: auto;
+  height: 100px;
+`;
 const Header = (props) => {
-    const history = useHistory();
-    console.log(props)
+  const history = useHistory();
+  console.log(props);
 
-    return (
-        <HeaderDiv>
-            <img src={Logo}></img>
-            <TitleContainer>{props.titulo}</TitleContainer>
-            <DivImagens>
-                <Imagem1 src={props.imagem1} onClick={() => goToHomePage(history)}></Imagem1>
-                <Imagem2 src={props.imagem2} onClick={() => goToPokedex(history)} ></Imagem2>
-            </DivImagens>
-  
-        </HeaderDiv>
-      );
-    };
+  return (
+    <HeaderDiv>
+      <HeaderContainer>
+        <LogoContainer>
+          {" "}
+          <LogoImg src={Logo}></LogoImg>
+        </LogoContainer>
+
+        <TitleContainer>{props.titulo}</TitleContainer>
+        <ImgContainer>
+          <Imagem1
+            src={props.imagem1}
+            onClick={() => goToHomePage(history)}
+          ></Imagem1>
+          <Imagem2
+            src={props.imagem2}
+            onClick={() => goToPokedex(history)}
+          ></Imagem2>
+        </ImgContainer>
+      </HeaderContainer>
+    </HeaderDiv>
+  );
+};
 
 export default Header;
-    
