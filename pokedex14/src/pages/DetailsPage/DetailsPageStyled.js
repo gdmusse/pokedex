0bb2be8d10fdce3a69ea5f46ad6c9b845ab6a34b
Danielle Costa <dascott.pucca@gmail.com>
2021-04-20 14:10:21 -0300

    adicionado a pasta Hooks e arquivo useRequestData, inclui o arquivo url na pasta routes e fiz algumas alteração na pagina Details

---
+++ pokedex14/src/pages/DetailsPage/DetailsPageStyled.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const DetailsCard = styled.div `
+width: 500px;
+display: flex;
+justify-content: center;
+flex-wrap: wrap;
+border-top: 1px solid rgba(255, 255, 255, 0.5);
+border-left: 1px solid rgba(255, 255, 255, 0.5);
+backdrop-filter: blur(5px);
+margin: auto;
+text-align: center;
+margin-top: 100px;
+color: white;
+margin-left: 30px;
+background-color: #1C1C21;
+border: 1px solid black;
+box-shadow: 20px 20px 50px rgba(0, 0, 0, 0.5);
+border-radius: 15px;
+background: rgba(255, 255, 255, 0.1);
+`
+
+export const Linha = styled.div `
+width: 100%
+`
